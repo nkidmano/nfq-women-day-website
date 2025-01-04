e268a5026790c5e83e33d3cf535796a72f4a6aad
tomdapchai <vothanhtam2407@gmail.com>
2025-01-04 23:30:20 +0700

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Sigmar_One, Quicksand } from "next/font/google";
 import "./globals.css";
 import { NameProvider } from "@/context/nameContext";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 
 const sigmarOne = Sigmar_One({
     weight: "400",
@@ -30,6 +31,7 @@ export default function RootLayout({
                 className={`${sigmarOne.variable} ${quicksand.variable} antialiased w-full h-fullr`}>
                 <NameProvider>{children}</NameProvider>
                 <Toaster />
+                <Analytics />
             </body>
         </html>
     );
